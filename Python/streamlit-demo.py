6fe43fad5fd275f609057e37863a0a8d2aedb9ab
Pranjal Roy Vishvakarma <pranjalvishwakarma786@gmail.com>
2023-01-12 14:38:16 +0530

    day-3 progress. will finish upto day 10 tomorrow

---
+++ Python/streamlit-demo.py
@@ -0,0 +1,6 @@
+import streamlit as st 
+
+st.header('Hello World! UwU!!')
+
+if st.button("OwO!! What's this?", type='primary'): st.write("This is a message UwU!!")
+else: st.write("What does that button do? UwU!!")
