cb5d6788b9af1ba71550303afda37ce61e5914a0
Pranjal Roy Vishvakarma <pranjalvishwakarma786@gmail.com>
2023-01-16 14:06:41 +0530

    Imported all data, plotted two sheets

--- Python/streamlitdash.py
+++ Python/streamlitdash.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
+
+df1 = pd.read_excel('/home/pranjalsmiling/Projects/CovidDataDashboard/SQLServerExport.xlsx', 0)
+df2 = pd.read_excel('/home/pranjalsmiling/Projects/CovidDataDashboard/SQLServerExport.xlsx', 1)
+df3 = pd.read_excel('/home/pranjalsmiling/Projects/CovidDataDashboard/SQLServerExport.xlsx', 2)
+df4 = pd.read_excel('/home/pranjalsmiling/Projects/CovidDataDashboard/SQLServerExport.xlsx', 3)
 
 
-totaldeathsdf = pd.read_excel('/home/pranjalsmiling/Projects/CovidDataDashboard/SQLServerExport.xlsx')
-st.write(totaldeathsdf)
+st.write(df1)
+
+c = alt.Chart(df2).mark_bar().encode( # type: ignore 
+    x='Continent', y='TotalDeathCount'
+)
+
+st.altair_chart(c)
